bb37423ba8f3bb96cf74a6060625a8a6d6c9fe97
corentin.chan <chancorentin@hotmail.com>
2025-12-10 21:09:43 +0100

    test

--- recipe-reactnode-app/src/components/account/recipesList.jsx
+++ recipe-reactnode-app/src/components/account/recipesList.jsx
@@ -48,9 +48,9 @@ const [recipes,setRecipes]=useState([])
         //setMessage(data.message);
         console.log("Réponse backend delete own :", data);
 
-        // if (data.check) {
-        // navigate(0); 
-        // }
+        if (data.check) {
+        navigate(0); 
+        }
       } catch (error) {
         console.error("Erreur lors de la connexion :", error);
          //setMessage(error.response?.data?.message || "Erreur réseau");

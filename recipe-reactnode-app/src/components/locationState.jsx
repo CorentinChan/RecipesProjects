1e8ece9e4c475f016b146e4cc7bb9a6a4c7186a5
corentin.chan <chancorentin@hotmail.com>
2025-12-10 15:57:42 +0100

    update

--- recipe-reactnode-app/src/components/locationState.jsx
+++ recipe-reactnode-app/src/components/locationState.jsx
@@ -1,33 +1,46 @@
 import { useState,useEffect } from 'react'
 import { useLocation } from "react-router-dom";
-
+import axios from 'axios';
 import { useNavigate } from "react-router-dom";
  
  export default function  LocationState({setLocationState,user,userRole,setUser,setUserRole,locationState}) {
       const location =useLocation();
 
-  useEffect(() => {
+  useEffect(async() => {
       console.log("API" + import.meta.env.VITE_API_URL);
+ try {
+          let pseudo = profil.pseudo;
+          let image= profil.image;
+          let description = profil.description;
+        const { data } = await axios.post(
+          import.meta.env.VITE_API_URL + "/getPseudo",
+          {
+          },
+          {
+            headers: {"Content-Type": "application/json",},
+            withCredentials: true,
+          }
+        );
 
-     async function getPseudo(){ 
-      const response = await fetch(import.meta.env.VITE_API_URL+"/getPseudo", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
+        setMessage(data.message);
+        console.log("Réponse backend :", data);
 
-         });
-
-    const data = await response.json();
-    console.log("Réponse backend app :", data);
+        if (data.message==="Profil udpated") {
+          //setUser(data.pseudo);
+          console.log("succeed")
+          //setProfilParent(profil);
+        navigate(0); 
+            console.log("Réponse backend app :", data);
         setUser(data.pseudo);
         setUserRole(data.role);
-         console.log('userrole'+user+userRole+locationState);
+         console.log("getpseudo and role");
+        
         }
-      getPseudo();
-      console.log('userrole'+user+userRole+locationState);
-      setLocationState(prev=>prev+1);
+      } catch (error) {
+        console.error("Erreur lors de la connexion :", error);
+         setMessage(error.response?.data?.message || "Erreur réseau");
+      }
+    
   }, [location.pathname,user]);
 
 

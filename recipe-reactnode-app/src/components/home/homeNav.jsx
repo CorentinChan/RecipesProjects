baa7846967ffd6954e573e031ced1b00bcf2dd93
corentin.chan <chancorentin@hotmail.com>
2025-12-16 02:30:59 +0100

    hom nav style

--- recipe-reactnode-app/src/components/home/homeNav.jsx
+++ recipe-reactnode-app/src/components/home/homeNav.jsx
@@ -11,7 +11,7 @@ return (
 
     <div className="accordion-item">
       <h2 className="accordion-header">
-        <button className="accordion-button fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true">
+        <button className="accordion-button bg-light fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true">
           Dishes
         </button>
       </h2>
@@ -32,7 +32,7 @@ return (
 
     <div className="accordion-item">
       <h2 className="accordion-header">
-        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false">
+        <button className="accordion-button bg-light collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false">
          World Food
         </button>
       </h2>
@@ -52,7 +52,7 @@ return (
 
     <div className="accordion-item">
       <h2 className="accordion-header">
-        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false">Others
+        <button className="accordion-button bg-light collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false">Others
         </button>
       </h2>
       <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#menuAccordion">

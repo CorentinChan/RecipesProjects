33ef8817776505797855d42c7444d31d4102aa5b
corentin.chan <chancorentin@hotmail.com>
2026-01-20 05:09:36 +0100

    maj fini

--- RecipeProjectjsreact/backend/app.js
+++ RecipeProjectjsreact/backend/app.js
@@ -1446,7 +1446,7 @@ app.get("/profile", async(req, res) => {
 });
 
 
-const job = schedule.scheduleJob('* * * * *', function(){
+const job = schedule.scheduleJob('3 3 * * *', function(){
 		console.log("MAJ API");
 		copyMealDBC(connection);
 	});

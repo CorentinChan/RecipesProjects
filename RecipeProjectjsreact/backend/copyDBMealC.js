21d3275e4c354150a1b7913f463376f65d3e4b2d
corentin.chan <chancorentin@hotmail.com>
2026-01-20 05:16:23 +0100

    corr

--- RecipeProjectjsreact/backend/copyDBMealC.js
+++ RecipeProjectjsreact/backend/copyDBMealC.js
@@ -28,10 +28,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'recipeproject3'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+	multipleStatements: true
 });
 
 connection.connect(err => {
